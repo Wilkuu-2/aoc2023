1c024cc57a920aa629eb2cd13f8f963bfac4d53c
Jakub Stachurski <jakub@wilkuu.xyz>
2023-12-02 22:55:54 +0100

    Make Hand and Game implement FromStr instead of using their own parse() function

--- src/day2.rs
+++ src/day2.rs
@@ -22,26 +22,19 @@ impl std::fmt::Display for Hand {
         write!(f,"r: {}, g: {}, b: {}", self.red, self.green ,self.blue)
     }
 }
-
-impl Hand {
-    fn new(red: usize, green: usize, blue: usize) -> Self {
-        Self{red,green,blue}
-    } 
-    fn fits_in(&self, max: &Self) -> bool{
-        self.red <= max.red && self.green <= max.green && self.blue <= max.blue 
+#[derive(Debug)]
+struct ParseError(String); 
+impl<'a> std::error::Error for ParseError {}
+impl<'a> std::fmt::Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f,"[{}]",self.0)
     }
+}
 
-    fn make_fit(&mut self, to_fit: &Self) {
-        self.red = self.red.max(to_fit.red);
-        self.green = self.green.max(to_fit.green);
-        self.blue = self.blue.max(to_fit.blue);
-    }
 
-    fn power(&self) -> usize {
-        self.red * self.blue * self.green
-    } 
-
-    fn parse(input: &str) -> Self {
+impl std::str::FromStr for Hand {
+    type Err = ParseError;
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
         let mut out = Self::new(0,0,0); 
         for val in input.split(",") {
             let mut num: u32  = 0; 
@@ -62,11 +55,31 @@ impl Hand {
                 "blue" => out.blue = count,
                 "red" =>  out.red = count, 
                 "green" => out.green = count, 
-                _ => panic!("Unmatched color {}", color)
+                _ => return Err(ParseError(format!("Unmatched color {}", color))),
             } 
         } 
-        out
+        Ok(out)
     }
+}
+
+impl Hand {
+    fn new(red: usize, green: usize, blue: usize) -> Self {
+        Self{red,green,blue}
+    } 
+    fn fits_in(&self, max: &Self) -> bool{
+        self.red <= max.red && self.green <= max.green && self.blue <= max.blue 
+    }
+
+    fn make_fit(&mut self, to_fit: &Self) {
+        self.red = self.red.max(to_fit.red);
+        self.green = self.green.max(to_fit.green);
+        self.blue = self.blue.max(to_fit.blue);
+    }
+
+    fn power(&self) -> usize {
+        self.red * self.blue * self.green
+    } 
+
 } 
 
 struct Game { 
@@ -74,19 +87,23 @@ struct Game {
     hands: Vec<Hand>
 }
 
-impl Game {
-    fn parse(line: &str) -> Option<Self> {
-       let id_re = Regex::new(r"Game ([0-9]+): (.*)").unwrap();
-       if let Some(capture) = id_re.captures(line) {
-           let (_, [id_str, state_str]) = capture.extract();
-           // println!("{} >> {}", id_str, state_str);
-           let id: usize = id_str.parse().unwrap_or(0);
-           let hands: Vec<Hand> = state_str.split(";").map(|s| Hand::parse(s)).collect(); 
-           Some(Game {id, hands})
-       } else {
-           None 
-       }
+impl std::str::FromStr for Game {
+    type Err = ParseError; 
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        let id_re = Regex::new(r"Game ([0-9]+): (.*)").unwrap();
+        if let Some(capture) = id_re.captures(line) {
+            let (_, [id_str, state_str]) = capture.extract();
+            // println!("{} >> {}", id_str, state_str);
+            let id: usize = id_str.parse().unwrap_or(0);
+            let hands: Vec<Hand> = state_str.split(";").map(|s| s.parse().unwrap()).collect(); 
+            Ok(Game {id, hands})
+        } else {
+            Err(ParseError(format!("Can't parse: \n {} \n is invalid syntax", line))) 
+        }
     }
+}
+
+impl Game {
 
     fn get_minimum_cubes(&self) -> Hand{
         let mut min = Hand::new(0,0,0);
@@ -119,7 +136,7 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
 fn part1(input: &str, max: Hand) -> usize {
     input.lines()
         .into_iter()
-        .map(|x| Game::parse(x).unwrap())
+        .map(|x| x.parse::<Game>().unwrap())
         .filter(|x| x.validate_against(max.clone()))
         .map(|x| x.id)
         .sum()
@@ -129,7 +146,7 @@ fn part2(input: &str) -> usize {
     input.lines()
         .into_iter()
         .map(|x| {
-            let game = Game::parse(x).unwrap(); 
+            let game: Game = x.parse().unwrap(); 
             let pow = game.get_minimum_cubes().power();
             //println!("{} -> {}", game.id, pow);
             pow
